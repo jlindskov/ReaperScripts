129533a68645a377458a9f573cbe76943ba8cf5b
Jeppe Lindskov <jeppelind@icloud.com>
2021-04-09 19:56:42 +0200

    Initial Commit

---
+++ ReaGames/JL_ReaSnek.lua
@@ -0,0 +1,432 @@
+-- @description ReaSnek
+-- @version 1.0.0
+-- @author Jeppe Emil Lindskov
+-- @about
+--   # ReaSnek
+--   Classic Snake, now available in Reaper!
+
+
+-- GLOBAL Variables ------------------------------
+
+BoardSizeX = 1000
+BoardSizeY = 1000
+PlayerSpeed = 20
+prevInputValue = 1
+prevInputDir = "X"
+currentDir = 0
+fruitList = {}
+noFruits = false
+Dir = nil
+score = 0
+
+
+-- Vector2 Class-------------------------------
+
+Vector2 = {x = 0,y = 0, width = 20, height = 20}
+
+function Vector2:new(o)
+o = o or {}
+setmetatable(o,self)
+self.__index = self
+return o
+end
+
+-- Player Class --------------------------------
+
+Player = {x = 0, y = 0,width = 20,heigth = 20, dead = false, snakePos = {}, snakeBodySize = 0,}
+
+function Player:Draw()
+  gfx.r = 1
+  gfx.b = 0
+  gfx.g = 1
+  gfx.rect(self.x,self.y,self.width,self.heigth)
+
+end
+  
+-- Fruit Class ---------------------------------  
+
+Fruit = {x = 25, y = 0, width = 25,heigth = 25, dead = false}
+  
+  
+function Fruit:Draw()
+
+  if self.dead == false then
+    gfx.r = 1
+    gfx.b = 0
+    gfx.g = 0
+    gfx.rect(self.x, self.y,self.width, self.heigth)
+  end
+end
+
+function Fruit:new(o)
+o = o or {}
+setmetatable(o,self)
+self.__index = self
+return o
+
+end
+
+
+-------------------------------------------------
+
+function CheckForInput()
+  char = gfx.getchar()
+  
+  if Player.dead == true then
+      if char == 32 then 
+          SpawnPlayer()
+      end
+  end
+  
+  if char == 100 then
+   return 1, true, "X"
+  end 
+  
+  if char == 97 then 
+   return -1, true, "X"
+  end
+  
+ if char == 119 then
+  return -1, true, "Y"
+ end 
+ 
+  if char == 115 then 
+  return 1, true, "Y"
+  end
+  
+if char == 27 then 
+      gfx.quit()
+  else
+    return 0 ,false, nil 
+  end
+end
+
+function KillPlayer()
+
+ Player.dead = true
+ 
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+ 
+
+function Update()
+
+  -- Check input and handle it correctly
+  inputValue,wasInput, inputDir = CheckForInput()
+  
+  -- If player is dead just bail out
+  if Player.dead == true then 
+    return 
+  end 
+  
+  -- If input is opposite, do nothing... 
+
+  if inputDir == prevInputDir and inputVaule ~= prevInputValue then 
+    inputValue = prevInputValue
+  end
+  
+  -- If user is not pressing the right key or no key
+  if wasInput == false then 
+    inputValue = prevInputValue
+    inputDir = prevInputDir
+  end
+  
+  
+  -- Store previous location so that tail updates correctly
+  local vector = Vector2:new()
+  vector.x = Player.x
+  vector.y = Player.y
+  -- Make the collision size small so we dont collide with our selves
+  vector.width = 1
+  vector.height = 1
+  table.insert(Player.snakePos,1,vector)
+  
+  
+  -- Move the head of the snake
+  if inputDir == "X" then
+    Player.x = Player.x + PlayerSpeed * inputValue
+    if inputValue == 1 then 
+       Dir = "Right"
+    else inputValue = -1
+        Dir = "Left"
+    end
+    prevInputValue = inputValue
+    prevInputDir = inputDir
+  end
+  
+  if inputDir == "Y" then 
+    Player.y = Player.y + PlayerSpeed * inputValue
+    
+    if inputValue == 1 then 
+      Dir = "Down"
+    else inputValue = -1
+      Dir = "Up"
+    end
+    prevInputValue = inputValue
+    prevInputDir = inputDir
+  end
+  
+  -- Move the tail portion 
+  Xcount = tablelength(Player.snakePos)
+  if Xcount >= Player.snakeBodySize +1 then 
+      -- If we have more prev position entries that we have tail we remove the oldest one
+      table.remove(Player.snakePos, Xcount)
+  end
+  
+  -- Check if we are out of bounds
+  CheckBounds()
+  
+  -- Check if we are overlapping a fruit 
+  CheckFruitOverlap(Player.x, Player.y, Player.width, Player.heigth,fruitList)
+
+  -- Check if we are overlapping our tail  
+for i,v in ipairs (Player.snakePos) do
+
+  -- Kinda big hack to make sure we dont collide with the first two points
+  if i ~= 1 and i ~= 2 then 
+    isInside = CheckTailOverLap(Player.x, Player.y, Player.width, Player.heigth, v)
+      if isInside then
+        KillPlayer()
+      end
+    end
+  end
+end
+
+
+
+function CheckBounds()
+  
+  if Player.x >= BoardSizeX -25 then
+    KillPlayer()
+  end
+  
+  if Player.x <= 0 +2  then
+    KillPlayer()
+  end
+  
+  if Player.y >= BoardSizeY -25 then
+   KillPlayer()
+  end
+  
+  if Player.y <= 0 +2 then 
+     KillPlayer()
+  end
+  
+end 
+
+
+
+function IsPointInsideBox(x,y,col)
+
+  isXInside = x>= col.x and x <= col.x + col.width
+  isYInside = y>= col.y and y <= col.y + col.height
+  
+  if isXInside and isYInside == true then 
+    return true  
+  else
+    return false
+  end
+
+end
+
+
+function CheckFruitOverlap(x,y,width,heigth,collisions)
+
+for i,v in ipairs (fruitList) do
+  if v.dead == false or v.dead == nil then
+    topLeftCol = IsPointInsideBox(x,y,v)
+    topRightCol = IsPointInsideBox(x + width,y,v)
+    botLeftCol = IsPointInsideBox(x, y + heigth,v)
+    botRightCol = IsPointInsideBox(x + width, y + heigth, v)
+    if topLeftCol or topRightCol or botLeftCol or botRightCol then 
+      -- for i = 1 ,10 do    
+      FruitCollected(v)
+      -- end
+      return true 
+    else 
+      return false
+    end
+  end
+end
+end
+
+
+function CheckTailOverLap(x,y,width,heigth,collisions)
+
+  -- The -2 is to give a tiny amount of leeway so we dont collide when going close to us self. 
+
+  topLeftCol = IsPointInsideBox(x,y,collisions)
+  topRightCol = IsPointInsideBox(x + width - 2 ,y,collisions)
+  botLeftCol = IsPointInsideBox(x, y + heigth -2 ,collisions)
+  botRightCol = IsPointInsideBox(x + width -2, y + heigth, collisions)
+  
+  if topLeftCol or topRightCol or botLeftCol or botRightCol then 
+    return true 
+  else 
+    return false
+  end
+end
+
+
+
+function FruitCollected(fruit)
+fruit.dead = true
+Player.snakeBodySize = Player.snakeBodySize + 1
+score = score + 100
+end
+
+
+
+function SpawnPlayer()
+Player.x = 500
+Player.y = 500
+Player.dead = false
+Player.snakeBodySize = 0
+Player.snakePos = {}
+
+CreateFruit()
+score = 0
+
+end
+
+-- Pre create fruits 
+function CreateFruit()
+
+for i = 1, 4 do 
+local f = Fruit:new()
+
+fruitList = {}
+
+f.x = 10
+f.y = 0
+f.height = 20 
+f.width = 20
+f.dead = true
+table.insert(fruitList,f)
+end
+
+
+end
+
+
+-- Spawn fruit at a random location
+function SpawnFruit()
+
+  local selFruit = nil
+  
+  for i,v in ipairs(fruitList)do
+    if v.dead == true then 
+    v.x = math.random(25,BoardSizeX - 25)
+    v.y = math.random(25,BoardSizeY - 25)
+    v.dead = false
+    break
+    
+    end
+  end
+end
+
+function DrawMenu()
+  
+  gfx.r = 1
+  gfx.b = 0
+  gfx.g = 1
+
+
+  gfx.x = 330
+  gfx.y = 250
+  gfx.setfont(1,"Arial", 100)
+  gfx.drawstr("ReaSnek")
+  
+  gfx.setfont(1,"Arial", 50)
+  
+  gfx.x = 425
+  gfx.y = 400
+  gfx.drawstr("Score: ")
+  
+  gfx.r = 1
+  gfx.b = 0
+  gfx.g = 0
+  
+  gfx.drawnumber(score,0)
+  
+  gfx.r = 1
+  gfx.b = 0
+  gfx.g = 1
+  
+  gfx.x = 315
+  gfx.y = 600
+  gfx.drawstr("Press space to play")
+  
+  gfx.setfont(1,"Arial", 25)
+  gfx.x = 700
+  gfx.y = 950
+  gfx.drawstr("WASD or Arrows to move")
+
+end
+
+
+function RenderGFX()
+
+  -- Render Player 
+  Player:Draw()
+
+
+  -- Render trail
+  gfx.r = 0
+  gfx.b = 1
+  gfx.g = 1
+  for i,v in ipairs (Player.snakePos) do
+      gfx.rect(v.x, v.y ,20,20)
+  end
+  
+  -- see if we have any active fruit. 
+  local counter = 0
+  for i,v in ipairs (fruitList) do
+    if v.dead == false then 
+    v:Draw()
+    counter = counter + 1
+    end
+  end
+  
+  -- Spawn a fruit if not fruit is visible
+  if counter == 0 then
+   SpawnFruit()
+  end
+  
+  -- If we are dead, show menu 
+  if Player.dead == true then
+    DrawMenu()
+  end
+  
+  
+end
+
+
+
+function MainLoop()
+
+  Update()
+  RenderGFX()
+  
+  -- Loop hack
+  if gfx.getchar() >= 0 then 
+    reaper.defer(MainLoop)
+  end 
+  
+end
+
+function Init()
+  screen = gfx.init("ReaSnek" ,BoardSizeX, BoardSizeY, 0,1000,500) 
+    Player.dead = true; 
+  GameStarted = true 
+end
+
+------------------------------------------------------------------------------------ 
+
+Init()
+MainLoop()
---
+++ ReaGames/JL_ReaSweeper.lua
@@ -0,0 +1,462 @@
+-- @description ReaSweeper
+-- @version 1.0.0
+-- @author Jeppe Emil Lindskov
+-- @about
+--   # ReaSweeper
+--   MineSweeper now available in Reaper
+
+-- GLOBAL Variables ------------------------------
+
+BoardSizeX = 500
+BoardSizeY = 1000
+score = 0
+
+gridWidth = 20
+gridHeight = 15
+cellHeight = 30
+cellwidth = 30
+numberOfMines = 25
+flaggedMines = numberOfMines
+gameEnded = false
+gameWon = false
+prevInput = 0
+selectedCell = nil
+grid = {}
+startTime = os.time() 
+currentTime = 0
+firstSelection = true
+
+
+-- Cell Class ---------------------------------  
+
+Cell = {x = 0, y = 0, width = 25, heigth = 25, mine = false, isRevealed = false, isFlagged = false, index ={x = 0, y = 0}, neighborMines = 0, cantBeMine = false}
+  
+  
+function Cell:Draw()
+  gfx.r = 0
+  gfx.b = 0
+  gfx.g = 0
+  gfx.rect(self.x, self.y ,self.width, self.height)
+ 
+  if self.isRevealed == true then
+  gfx.r = 0.7 
+  gfx.b = 0.7
+  gfx.g = 0.7
+  gfx.rect(self.x + 1, self.y + 1 ,self.width, self.height -1)
+  elseif self.isFlagged == true then
+  gfx.r = 1 
+  gfx.b = 0
+  gfx.g = 1
+  gfx.rect(self.x + 1, self.y + 1 ,self.width, self.height -1)
+  else
+    if self == selectedCell then 
+    gfx.r = 0.3 
+    gfx.b = 0.3
+    gfx.g = 0.3
+    else
+    gfx.r = 0.5
+    gfx.b = 0.5
+    gfx.g = 0.5
+    end
+    gfx.rect(self.x + 1, self.y + 1 ,self.width, self.height -1)
+  end
+ 
+if self.neighborMines > 0 and self.isRevealed == true then
+  gfx.r = 0 
+  gfx.b = 0
+  gfx.g = 0
+  
+  gfx.x = self.x +10
+  gfx.y = self.y + 5
+  gfx.setfont(1,"Arial", 20)
+  gfx.drawnumber(self.neighborMines,0)
+end
+ 
+ 
+ if self.mine == true and self.isRevealed == true then
+ 
+ if gameWon == true then 
+ 
+ gfx.r = 0
+ gfx.b = 0
+ gfx.g = 1
+ else 
+ gfx.r = 1 
+ gfx.b = 0
+ gfx.g = 0
+ end
+  gfx.rect(self.x + 1, self.y + 1 ,self.width, self.height -1)
+  
+  gfx.r = 0 
+  gfx.b = 0
+  gfx.g = 0
+  gfx.x = self.x +10
+  gfx.y = self.y + 5
+  gfx.setfont(1,"Arial", 20)
+  gfx.drawstr("B") 
+
+  end
+end  
+
+
+function Cell:new(o)
+  o = o or {}
+  setmetatable(o,self)
+  self.__index = self 
+  return o
+end
+
+
+
+function FlagForNoMines(index)
+
+grid[index.x][index.y].cantBeMine = true
+
+end
+---------------------------------------------------
+
+function RevealCell(index)
+    
+  -- If neighbourMines = 0 for the touched cell then
+  -- we reveal the neighbour cells as well using a 
+  -- recursive call to this function.
+  
+if firstSelection == true then
+
+  markNeighbourCells(index.x - 1, index.x + 1, index.y - 1, index.y + 1) 
+  CreateMines()
+  firstSelection = false 
+    
+
+end
+  
+  
+  grid[index.x][index.y].isRevealed = true
+  if grid[index.x][index.y].mine == true then
+  RevealGameBoard()
+  gameEnded = true
+    return
+  end
+    if grid[index.x][index.y].neighborMines == 0 then
+      inNeighbourCells(index.x - 1, index.x + 1, index.y - 1, index.y + 1, function(x, y) 
+          if not grid[x][y].isRevealed and not grid[x][y].mine and not grid[x][y].flagged then
+              RevealCell({x = x, y = y})
+          end
+    end)
+    end
+end
+
+function CreateCell(i,j,x,y)
+  local cell = Cell:new()
+  cell.x = x
+  cell.y = y
+  cell.index = {x = i, y = j}
+  cell.width = cellwidth
+  cell.height = cellHeight
+  cell.mine = false
+  cell.isRevealed = false
+  cell.neighborMines = 10
+  cell.isFlagged = false
+  cell.cantBeMine = false
+  return cell
+end
+
+
+function DrawGrid()
+    
+  -- Iterate through the grid matrix and draw each cell
+  for i = 1, gridWidth do
+    for j = 1, gridHeight do
+        grid[i][j]:Draw()
+    end
+  end
+
+end
+
+
+function RevealGameBoard()
+    for i = 1, gridWidth do
+      for j = 1, gridHeight do
+          grid[i][j].isRevealed = true
+      end
+    end
+end 
+
+
+function DrawSidePanel()
+
+  
+  --Panel BG
+  gfx.r = 0.6 
+  gfx.b = 0.6
+  gfx.g = 0.6
+  gfx.rect( BoardSizeX - 200, 15, 190, 200)
+  gfx.x = BoardSizeX - 195
+  gfx.y = 20
+  gfx.r = 0 
+  gfx.b = 0
+  gfx.g = 0
+  
+  --Bomb Calculation
+  gfx.setfont(1,"Arial", 30)
+  gfx.drawstr("Bombs " .. tostring(flaggedMines).."/"..tostring(numberOfMines))
+  
+  --Timer
+  gfx.x = BoardSizeX - 195
+  gfx.y = 50
+  
+  if gameEnded == false then 
+  currentTime = os.difftime(os.time(), startTime)
+  end
+  gfx.drawstr("Time: "..tostring(currentTime)) 
+  
+  gfx.x = BoardSizeX - 195
+  gfx.y = 100
+  gfx.setfont(1,"Arial", 25)
+  
+  if gameEnded == true then 
+  
+    if gameWon == true then 
+      gfx.drawstr("GAME WON! :D") 
+    else
+      gfx.drawstr("GAME LOST! x(") 
+    end
+  end
+end
+
+
+function ResetTimer()
+currentTime = 0
+startTime = os.time()
+end
+
+
+function inNeighbourCells(startX, endX, startY, endY, closure)
+  for i = math.max(startX, 1), math.min(endX, gridWidth) do
+      for j = math.max(startY, 1), math.min(endY, gridHeight) do
+          closure(i, j)
+      end
+  end
+end
+
+function markNeighbourCells(startX, endX, startY, endY)
+  for i = math.max(startX, 1), math.min(endX, gridWidth) do
+      for j = math.max(startY, 1), math.min(endY, gridHeight) do
+      grid[i][j].cantBeMine = true
+          --closure(i, j)
+      end
+  end
+end
+
+
+function countMines(index)
+    
+  local mineNum = 0
+    inNeighbourCells(index.x - 1, index.x + 1, index.y - 1, index.y + 1, 
+        function(x, y) if grid[x][y].mine then mineNum = mineNum + 1 end
+        end)
+    return mineNum
+end
+
+
+function CreateGrid()
+
+  local baseX =  gridWidth
+  local y =  gridHeight
+  local x = baseX
+
+  for i = 1, gridWidth do
+    grid[i] = {}     -- create a new row
+      for j = 1, gridHeight do
+        grid[i][j] = CreateCell(i, j, x, y)
+        x = x + cellHeight
+        BoardSizeX = x
+      end
+    x = baseX
+    y = y + cellwidth  
+    BoardSizeY = y
+  end
+  BoardSizeY = BoardSizeY + 10
+  BoardSizeX = BoardSizeX +210
+  
+ -- CreateMines()
+    
+  for i = 1, gridWidth do
+    for j = 1, gridHeight do
+      grid[i][j].neighborMines = countMines(grid[i][j].index)
+    end
+  end
+end
+
+
+function CreateMines()
+
+  for i = 1, numberOfMines do
+      local mineX, mineY
+        repeat
+          mineX = math.random(1, gridWidth)
+          mineY = math.random(1, gridHeight)
+        until grid[mineX][mineY].mine == false or grid[mineX][mineY].cantBeMine == false     -- we dont want to duplicate mine location
+        grid[mineX][mineY].mine = true
+  end
+  
+  for i = 1, gridWidth do
+     for j = 1, gridHeight do
+       grid[i][j].neighborMines = countMines(grid[i][j].index)
+     end
+   end
+
+
+end
+
+
+function RenderBG()
+  gfx.r = 0.3
+  gfx.r = 0.3
+  gfx.b = 0.3
+  gfx.g = 0.3 
+  gfx.rect(0,0,BoardSizeX,BoardSizeY)
+end
+
+function RenderGFX()
+  RenderBG()
+  DrawGrid()
+  DrawSidePanel()
+  
+end
+
+
+function GetCell(x,y)
+
+  for i = 1, gridWidth do
+        for j = 1, gridHeight do
+        if grid[i][j].x < x and x < grid[i][j].x + grid[i][j].width 
+        and grid[i][j].y < y and y < grid[i][j].y + grid[i][j].height then
+          return grid[i][j]
+        end
+      end
+  end
+  return nil
+end
+
+ lb_down = function() return gfx.mouse_cap&1 == 1 end
+ rb_down = function() return gfx.mouse_cap&2 == 2 end
+ last_LMB_state = false
+ last_RMB_state = false
+ 
+ function OnMouseDown(x, y, lmb_down, rmb_down)
+   -- LMB clicked
+   if not rmb_down and lmb_down and last_LMB_state == false then
+    last_LMB_state = true
+    cell = GetCell(gfx.mouse_x,gfx.mouse_y)
+    selectedCell = cell
+   end
+   -- RMB clicked
+   if not lmb_down and rmb_down and last_RMB_state == false then
+     last_RMB_state = true
+    cell = GetCell(gfx.mouse_x,gfx.mouse_y)
+    selectedCell = cell
+   end
+ end
+ 
+ 
+function OnMouseUp(x, y, lmb_down, rmb_down)
+  if not lmb_down and last_LMB_state then
+    last_LMB_state = false 
+    --cell = GetCell(gfx.mouse_x,gfx.mouse_y)
+    cell = selectedCell
+  if cell ~= nil then
+       RevealCell(cell.index)
+  end
+
+end
+
+  if not rmb_down and last_RMB_state then
+    last_RMB_state = false
+    --cell = GetCell(gfx.mouse_x,gfx.mouse_y)
+    cell = selectedCell
+    if cell ~= nil then
+      if  cell.isFlagged == false then
+        cell.isFlagged = true
+        flaggedMines = flaggedMines - 1
+        else
+        cell.isFlagged = false
+        flaggedMines = flaggedMines + 1
+        end
+      end
+   end
+ selectedCell = nil
+end
+
+
+function CheckWinCondition()
+
+unopenedCells = 0 
+
+
+ for i = 1, gridWidth do
+    for j = 1, gridHeight do
+    if grid[i][j].isRevealed == false or grid[i][j].isFlagged then 
+        unopenedCells = unopenedCells + 1
+      end
+    end
+  end
+
+  if unopenedCells == numberOfMines then 
+     gameWon = true
+     gameEnded = true
+     RevealGameBoard()
+    
+  end 
+end
+
+function GetMouseInput()
+  
+  local LB_DOWN = lb_down()           -- Get current left mouse button state
+  local RB_DOWN = rb_down() 
+  
+  if (LB_DOWN and not RB_DOWN) or (RB_DOWN and not LB_DOWN) then   -- LMB or RMB pressed down?
+    if (last_LMB_state == false and not RB_DOWN) or (last_RMB_state == false and not LB_DOWN) then
+      OnMouseDown(mx, my, LB_DOWN, RB_DOWN)
+    end
+  elseif not LB_DOWN and last_RMB_state or not RB_DOWN and last_LMB_state then
+      OnMouseUp(mx, my, LB_DOWN, RB_DOWN)
+  end
+end
+ 
+ 
+function MainLoop()
+
+ -- Update()
+  GetMouseInput()
+
+  RenderGFX()
+  CheckWinCondition()
+  
+  -- Loop hack
+  if gfx.getchar() >= 0 then 
+    reaper.defer(MainLoop)
+  end 
+  
+end
+
+function Init()
+
+  retval,retvalCSV  = reaper.GetUserInputs("Welcome To ReaSweeper", 3, "Grid Width:,Grid Height:,Mines:", "10,15,20")
+  if retval ~= false then
+  gridWidth, gridHeight, numberOfMines = string.match(retvalCSV, "([^,]+),([^,]+),([^,]+)")
+  gridWidth = tonumber(gridWidth)
+  gridHeight = tonumber(gridHeight)
+  numberOfMines = tonumber(numberOfMines)
+  flaggedMines = numberOfMines
+  CreateGrid()
+  screen = gfx.init("ReaBird" ,BoardSizeX, BoardSizeY, 0,1000,500)
+  ResetTimer()
+  MainLoop()
+  
+  end
+end
+
+------------------------------------------------------------------------------------ 
+
+Init()
